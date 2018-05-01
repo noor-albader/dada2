f845e7beaa8fe6d851b2e3a813d010f308e59cc4
bakerkri <38112445+bakerkri@users.noreply.github.com>
2018-05-01 09:35:40 -0700

    Update chimeras_tax.R
    fixed database files and added species assignment

--- chimeras_tax.R
+++ chimeras_tax.R
@@ -1,8 +1,7 @@
 ##Removes chimeras and assigns taxonomy
-##will need to get Silva db
-##See https://www.arb-silva.de/projects/ssu-ref-nr/ , under downloads, want archive fastq file
-## believe found here https://www.arb-silva.de/no_cache/download/archive/release_132/Exports/
-## titled SILVA_132_SSURef_Nr99_tax_silva.fasta.gz
+##Uses Silva database
+## maintained and formatted by dada2
+## https://benjjneb.github.io/dada2/training.html
 
 #! /usr/bin/env Rscript
 
@@ -12,8 +11,11 @@ library(dada2)
 
 # Remove chimeras
 seqtab <- removeBimeraDenovo(st.all, method="consensus", multithread=TRUE)
-# Assign taxonomy
-tax <- assignTaxonomy(seqtab, "/ACTF/Course/mb599_bds_s18/data/share/group3_Rothenberg/SILVA_132_SSURef_Nr99_tax_silva.fasta.g", multithread=TRUE)
+# Assign taxonomy to genus level, using training set
+tax <- assignTaxonomy(seqtab, "/ACTF/Course/mb599_bds_s18/data/share/group3_Rothenberg/silva_nr_v132_train_set.fa.gz", multithread=TRUE)
+# Add species level assignment to taxonomy table
+taxa <- addSpecies(taxa, "/ACTF/Course/mb599_bds_s18/data/share/group3_Rothenberg/silva_species_assignment_v132.fa.gz")
+
 # Write to disk
 saveRDS(seqtab, "/ACTF/Course/mb599_bds_s18/data/share/group3_Rothenberg/seqtab_final.rds") # CHANGE ME to where you want sequence table saved
 saveRDS(tax, "/ACTF/Course/mb599_bds_s18/data/share/group3_Rothenberg/tax_final.rds") 
